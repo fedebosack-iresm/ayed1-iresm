53e76d8c69ec75c8bdca25d5c4d797cb781b8b0f
Fede Bosack <federicobosack@iresm.edu.ar>
2021-08-06 11:18:05 -0300

    second commit

---
+++ Unidad 1/hello_world.py
@@ -0,0 +1,9 @@
+"""
+print("indique el primer argumento")
+primer_argumento = input()
+print("indique el segundo argumento")
+segundo_argumento = input()
+print(f"La suma de los dos argumentos es: {primer_argumento+segundo_argumento}")
+"""
+variable=str(3)
+print(type(variable))
--- hello_world.py
+++
@@ -1,9 +0,0 @@
-#Hello World 1°Repo
-def hello_world():
-    print("Hola mundo!")
-
-def suma(a,b):
-    return a+b
-
-hello_world()
-print(f"la suma de 2 y 3 es: {suma(2,3)}")
