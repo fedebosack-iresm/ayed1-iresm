1e125fdc301abf24a2719d53133e94f1ae976569
Fede Bosack <federicobosack@iresm.edu.ar>
2021-08-29 22:14:35 -0300

    agregadas unidad 2 y unidad 3

--- Unidad 1/hello_world.py
+++
@@ -1,9 +0,0 @@
-"""
-print("indique el primer argumento")
-primer_argumento = input()
-print("indique el segundo argumento")
-segundo_argumento = input()
-print(f"La suma de los dos argumentos es: {primer_argumento+segundo_argumento}")
-"""
-variable=str(3)
-print(type(variable))
